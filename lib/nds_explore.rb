15df012524d8fe058ca18ae08751a792b24ce30d
concurrent-server-0630 <andycruz008@gmail.com>
2020-02-14 04:58:45 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,7 +9,7 @@ end
 
 def print_first_directors_movie_titles
 pp directors_database
- directors_database[name].each do |title,value|
+ directors_database.each do |title,value|
    print value
  
 end
